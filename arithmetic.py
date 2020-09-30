377de5b1c3c3dd1dbef69d794c96c0ca69a7a6c6
Jenny Tan <tanjenny.yo@gmail.com>
2020-09-30 19:19:33 +0000

    fixed arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,41 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-
-    return 10
+    sum = num1 + num2
+    return sum
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    sub = num1 - num2
+    return sub
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    mul = num1 * num2
+    return mul
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    div = num2/num2
+    return div
 
 def square(num1):
     """Return the square of the input."""
-
+    squ = num1^2
+    return squ
 
 def cube(num1):
     """Return the cube of the input."""
-
+    cub = num1^3
+    return cub
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    pow = num1 ^ num2
+    return pow
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    rem = num1%num2
+    return rem
